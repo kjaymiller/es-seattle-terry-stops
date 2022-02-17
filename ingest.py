ddf1dbf8ae1a2e387ca4e300c0d54e648e738b56
Jay Miller <kjaymiller@gmail.com>
2022-02-17 11:01:02 -0800

    resolve -1 manipulation #1

--- ingest.py
+++ ingest.py
@@ -12,35 +12,33 @@ with open("socrata.json") as f:
     soda_info = json.load(f)
 
 
-def is_truthy(value: str, skip: Optional = None) -> Optional[bool]:
-    """Checks for skippable values and returns a boolean"""
-    if skip and value == skip:
-        return None
-    return value.lower() in ["Y", "true", "yes", "1"]
-
-
 def bulk_mapper(soda_client: Sequence) -> dict[str:str]:
-#    soda_data = soda_client.get(
-#            soda_info["dataset_id"],
-#            limit=10,
-#            order="terry_stop_id DESC",
-#    )  # loads partial data for testing
+    #    soda_data = soda_client.get(
+    #            soda_info["dataset_id"],
+    #            limit=10,
+    #            order="terry_stop_id DESC",
+    #    )  # loads partial data for testing
 
-    soda_data= soda_client.get_all(soda_info["dataset_id"],)
+    soda_data = soda_client.get_all(
+        soda_info["dataset_id"],
+    )
 
     for row in soda_data:
         data = {slugify(x, separator="_"): y for x, y in row.items()}
         new_row = {}
 
         for key, val in data.items():
+            if val == "-1":
+                continue
+
             val = val.lstrip("-")
 
-            if val.lower() not in ["", "-", "none"] and key != "weapon_type":
+            if val.lower() not in ["", "-", "none"] or key == "weapon_type":
                 # we want to include None values for Weapon Type
                 new_row[key] = val
 
             if key in ["friskflag", "arrestflag"]:
-                new_row[key] = is_truthy(val)
+                new_row[key] = val.lower() in ["y", "true", "yes", "1"]
 
         yield new_row
 
@@ -71,7 +69,6 @@ def load(index: Optional[str] = None) -> None:
         client, bulk_mapper(soda_client), index=index, thread_count=10
     ):
         pass
-
 
 if __name__ == "__main__":
     load(index="seattle-terry-stops")
